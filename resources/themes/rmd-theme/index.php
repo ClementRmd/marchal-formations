32a53610d0025bd0cd347ba056687e820e9ccb0f
Clément RAYMOND <clementraymond@MacBook-Air.home>
2021-10-17 16:41:10 -0400

    upd

--- resources/themes/rmd-theme/index.php
+++ resources/themes/rmd-theme/index.php
@@ -2,17 +2,6 @@
 
 get_header();
 
-if ( have_posts() ) :
-  while ( have_posts() ) :
-    the_post();
 
-    get_template_part('templates/home/home', 'hero');
-    get_template_part('templates/home/home', 'section');
-    get_template_part('templates/home/home', 'slider');
-
-  endwhile;
-endif;
-
-wp_reset_postdata();
 
 get_footer();
--- resources/themes/rmd-theme/single.php
+++ resources/themes/rmd-theme/single.php
@@ -2,6 +2,6 @@
 
 get_header();
 
-get_template_part('templates/single/single', 'content');
+//get_template_part('templates/single/single', 'content');
 
 get_footer();
