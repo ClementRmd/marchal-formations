0b4b95bc157a3e844f863827155ee66c44c0fcd7
Clement RAYMOND <clement.rmd@yahoo.com>
2021-11-08 19:52:01 -0400

    active debug

--- wp-config.php
+++ wp-config.php
@@ -88,7 +88,7 @@ $table_prefix = 'wp_';
  *
  * @link https://fr.wordpress.org/support/article/debugging-in-wordpress/
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 define('WP_CACHE', true);
 // to force chmod file
 define('FS_CHMOD_DIR', (0755 & ~ umask()));
