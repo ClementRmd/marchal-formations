126c143d6327df86a9ac944a491f98fee9565df5
Clement RAYMOND <clement.rmd@yahoo.com>
2021-11-13 18:59:07 -0400

    upd

--- resources/themes/rmd-theme/index.php
+++ resources/themes/rmd-theme/index.php
@@ -3,5 +3,6 @@
 get_header();
 
 get_template_part('templates/home/home', 'hero');
+get_template_part('templates/home/home', 'trainings');
 
 get_footer();
---
+++ resources/themes/rmd-theme/templates/home/home-trainings.php
@@ -0,0 +1,31 @@
+<div class="home-trainings">
+    <div class="container">
+        <div class="title-section">
+            <h2>
+                Nos formations
+            </h2>
+        </div>
+        <div class="home-trainings__bloc">
+            <div class="home-trainings__bloc__text">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Vivamus eget lacus vitae metus luctus semper.
+                    Nam rhoncus ligula ex, vel blandit tortor porttitor at.
+                    Quisque fringilla a urna sed rhoncus.
+                    Praesent quis ullamcorper lectus, a vehicula purus.
+                    Fusce aliquam nisi ac tempus semper.
+                    Ut a tellus ac velit iaculis tempus.
+                </p>
+            </div>
+            <div class="home-trainings__bloc__selection">
+                <ul>
+                    <li><a href="#" title="#">Nos formations Nos formations</a></li>
+                    <li><a href="#" title="#">Nos formations</a></li>
+                    <li><a href="#" title="#">Nos formations</a></li>
+                    <li><a href="#" title="#">Nos formations</a></li>
+                    <li><a href="#" title="#">Nos formations</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
